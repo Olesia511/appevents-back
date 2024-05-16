8b59423e8cd23808e1fd7623bd5d4942633d6d69
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-05-16 19:24:42 +0300

    change createParticipant ( error 409)

--- controllers/participantsController.js
+++ controllers/participantsController.js
@@ -1,9 +1,18 @@
+import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import { Participant } from "../models/Participant.js";
 
 const createParticipant = async (req, res) => {
-  const addedParticipant = await Participant.create({ ...req.body });
-  res.status(201).json(addedParticipant);
+  const { email, idEvent } = req.body;
+  const result = await Participant.find({ idEvent, email }, "-createdAt -updatedAt").populate("event", null, null, {
+    strictPopulate: false,
+  });
+  if (result.length > 0) throw HttpError(409, "You are already registered for this event");
+
+  if (result.length === 0) {
+    const addedParticipant = await Participant.create({ ...req.body });
+    res.status(201).json(addedParticipant);
+  }
 };
 
 const getAllParticipants = async (req, res) => {
